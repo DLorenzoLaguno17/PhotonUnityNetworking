07ca0f05a7e439ed086073c5f41668f21df5a17a
Dani Lorenzo <dlorenzolaguno17@gmail.com>
2021-01-05 17:29:50 +0100

    prepared to build and continued working with main menu

---
+++ PUN2/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1.0f;
+
+    public void LoadNextLevel()
+    {
+        if (SceneManager.GetActiveScene().name == "PostCombat")
+            StartCoroutine(LoadLevel(0));
+        else
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
--- PUN2/Assets/Scripts/MenuManager.cs
+++ PUN2/Assets/Scripts/MenuManager.cs
@@ -1,22 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Variables
+    private LevelLoader loader;
+    private Image controlsImage;
+    private Text creditsText;
+    private Text titleText;
+
+    private Button playButton;
+    private Button creditsButton;
+    private Button creditsBackButton;
+    private Button controlsButton;
+    private Button controlsBackButton;
+
+    // -----------------------------
+    // CORE
+    // -----------------------------
+
+    void Awake()
     {
-        
+        loader = GameObject.FindGameObjectWithTag("LevelLoader").GetComponent<LevelLoader>();
+        controlsImage = GameObject.Find("ControlsImage").GetComponent<Image>();
+        creditsText = GameObject.Find("CreditsText").GetComponent<Text>();
+        titleText = GameObject.Find("TitleText").GetComponent<Text>();
+
+        playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        creditsButton = GameObject.Find("CreditsButton").GetComponent<Button>();
+        creditsBackButton = GameObject.Find("CreditsBackButton").GetComponent<Button>();
+        controlsButton = GameObject.Find("ControlsButton").GetComponent<Button>();
+        controlsBackButton = GameObject.Find("ControlsBackButton").GetComponent<Button>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // -----------------------------
+    // BUTTON FUNCTIONS
+    // -----------------------------
 
-    // Button functions
     public void PlayButton()
     {
 
@@ -24,6 +46,45 @@ public class MenuManager : MonoBehaviour
 
     public void CreditsButton()
     {
+        HideMenu();
+    }
 
+    public void ControlsButton()
+    {
+        HideMenu();
+    }
+
+    public void CreditsBackButton()
+    {
+        creditsBackButton.gameObject.SetActive(false);
+        creditsText.gameObject.SetActive(false);
+
+        ShowMenu();
+    }
+
+    public void ControlsBackButton()
+    {
+        controlsBackButton.gameObject.SetActive(false);
+        controlsImage.gameObject.SetActive(false);
+
+        ShowMenu();
+    }
+
+    // -----------------------------
+    // TOOLS
+    // -----------------------------
+
+    private void HideMenu()
+    {
+        playButton.gameObject.SetActive(false); 
+        creditsButton.gameObject.SetActive(false); 
+        controlsButton.gameObject.SetActive(false);
+    }
+
+    private void ShowMenu()
+    {
+        playButton.gameObject.SetActive(true);
+        creditsButton.gameObject.SetActive(true);
+        controlsButton.gameObject.SetActive(true);
     }
 }
