025f03fb51c38f1ea95dcf28fe5289e090462743
Dani Lorenzo <dlorenzolaguno17@gmail.com>
2021-01-25 14:32:33 +0100

    fixed bug when going to main menu from combat scene

--- PUN2/Assets/Pause.cs
+++ PUN2/Assets/Pause.cs
@@ -2,13 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
 
 public class Pause : MonoBehaviour
 {
-    public GameObject pausemenu;  
+    public GameObject pausemenu;
+    private GameObject otherPlayer;
+    private byte LEFT_ROOM_EVENT = 7;
+    private float lastTime = 0;
+
+    private void Start()
+    {
+        lastTime = Time.time;
+    }
+
+    // Event handlers
+    private void OnEnable()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived += LeaveSceneEvent;
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived -= LeaveSceneEvent;
+    }
+
+    private void LeaveSceneEvent(EventData obj)
+    {
+        if (obj.Code == LEFT_ROOM_EVENT)
+            Destroy(otherPlayer);
+    }
 
     void Update()
     {
+        if (Time.time - lastTime > 0.5f)
+        {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+            if (players.Length >= 2)
+            {
+                if (PhotonNetwork.IsMasterClient)
+                    otherPlayer = players[1].gameObject;
+                else
+                    otherPlayer = players[0].gameObject;
+            }
+        }
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
@@ -21,10 +60,8 @@ public class Pause : MonoBehaviour
 
                 pausemenu.SetActive(true);
                 Time.timeScale = 0;
-            }
-            
+            }            
         }
-
     }
 
     public void Resume() 
@@ -36,8 +73,12 @@ public class Pause : MonoBehaviour
     public void MainMenu()
     {
         Time.timeScale = 1;
-        GameObject.FindGameObjectWithTag("LevelLoader").GetComponent<LevelLoader>().LoadSceneByName("MainMenu", false);
+
+        object[] datas = new object[] { "minor" };
+        PhotonNetwork.RaiseEvent(LEFT_ROOM_EVENT, datas, RaiseEventOptions.Default, SendOptions.SendReliable);
+        
         PhotonNetwork.LeaveRoom();
+        GameObject.FindGameObjectWithTag("LevelLoader").GetComponent<LevelLoader>().LoadSceneByName("MainMenu", false);
     }
 
     public void Quit()
